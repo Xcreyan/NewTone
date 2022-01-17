fa8b3a721ffd44842fc6bdeeaccda29f90cb1ff7
Михаил Устьянцев <xcreyan@gmail.com>
2022-01-17 17:05:18 +0500

    Добавьте файлы проекта.

---
+++ NewTone/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NewTone
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            int j = Convert.ToInt32(textBox2.Text);
+            double x = Convert.ToInt32(textBox1.Text);
+            for (int i = 1; i < j; i++)
+            {
+
+            }
+        }
+    }
+}
